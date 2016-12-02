f830168ecdf0f5ef0aa7b9892b9a8d72489f55f0
jie <fgnna@zuqiukong.com>
2016-12-02 11:43:57 +0800

    s

---
+++ basicConcepts.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <cstdlib>
+
+void printSomething();
+void printSomethingForParams(int x);
+int add3(int x);
+int add3(double x);
+int defaultValuesParameters(int x,int y=10, int z=11);
+
+int main()
+{
+    //std::cout << "Hello, World!" ;
+    /*
+     std::cout << "Hello, World!" ;
+     */
+    /**
+     * std::cout << "Hello, World!" ;
+     */
+
+    std::cout << "Hello, World!" ;
+    std::cout << "Hello, World!\n" ;
+    std::cout << "Hello" << ", " << "World!" << std::endl;
+
+    int myVariable = 10;
+    std::cout << myVariable << std::endl;
+
+    int a = 100,b = 10;
+    int sum = a + b;
+    std::cout << a << "+" << b << "=" << sum << std::endl;
+    int c;
+    std::cout << "input a number:";
+    //std::cin >> c;
+    std::cout << "\nnumber=" << c << std::endl;
+
+
+    if(true)
+    {
+        std::cout << "if true" << std::endl;
+    }
+    else
+    {
+        std::cout << "if else" << std::endl;
+    }
+
+    int conut = 2;
+    while(conut-- != 0)
+    {
+        std::cout << "while:" << conut << std::endl;
+    }
+
+    int aa;
+    float bb;
+    double cc;
+    char test = 'S';//ASCII
+    std::string dd;
+    bool is = true;//1=true,0=false
+    int arraya[5] = {5,5,5,5,5};
+    arraya[0] = 5;
+    int arrayc[] = {5,5,5,5,5};
+    std::cout << arraya[2] ;
+    int arrayd[2][2] = {{1,2},{1,2}};
+    //pointers
+    int score = 5;
+    int *scorePointer = &score;
+    std::cout << &score << std::endl;
+    std::cout << scorePointer << std::endl;
+    std::cout << *scorePointer << std::endl;
+
+    *scorePointer = score + 3;
+    std::cout << *scorePointer << std::endl;
+    *scorePointer = *scorePointer + 3;
+    std::cout << *scorePointer << std::endl;
+
+    int *intPointer = new int;
+    std::cout << intPointer << std::endl;
+    intPointer = new int;
+    std::cout << intPointer << std::endl;
+    *intPointer = 4;
+    std::cout << *intPointer << std::endl;
+    std::cout << intPointer << std::endl;
+    std::cout << "delete" << std::endl;
+    delete intPointer;
+    std::cout << scorePointer << std::endl;
+    std::cout << intPointer << std::endl;
+
+    int *pp = NULL;
+    std::cout << pp << std::endl;
+    pp = new int[10]{1,1,1,1,1,1,1,1,1,1};
+    std::cout << "pp size " << sizeof(pp) << std::endl;
+    std::cout << "*pp size " << sizeof(*pp) << std::endl;
+    std::cout << "char size " << sizeof(char) << std::endl;
+    std::cout << "float size " << sizeof(float) << std::endl;
+    std::cout << "double size " << sizeof(double) << std::endl;
+    std::cout << "int size " << sizeof(int) << std::endl;
+    std::cout << "string size " << sizeof(std::string) << std::endl;
+    std::cout << "arrayd size " << sizeof(arrayd) << std::endl;
+    delete [] pp;
+    printSomething();
+    printSomethingForParams(3);
+    std::cout << "add3=" << add3(9) << std::endl;
+
+
+    std::cout << "Random Numbers: rand() = " << rand() << std::endl;
+    srand(98);
+    std::cout << "srand(98) = " << rand() << std::endl;
+    std::cout << "time(0) = " << time(0) << std::endl;
+    srand(time(0));
+    std::cout << "srand(time(0)) = " << rand() << std::endl;
+    std::cout << "defaultValuesParameters(4) = " << defaultValuesParameters(4) << std::endl;
+    std::cout << "overloading add3(4.78) = " << add3(4.78) << std::endl;
+
+    std::cout << "end ===============================================" << std::endl;
+    return 0;
+}
+
+void printSomething()
+{
+    std::cout << "printSomething()" << std::endl;
+}
+
+void printSomethingForParams(int x)
+{
+    std::cout << "printSomethingForParams(int x) = " << x << std::endl;
+}
+
+int add3(int x)
+{
+    return x += 3;
+}
+int add3(double x)
+{
+    return x += 3;
+}
+
+int defaultValuesParameters(int x,int y,int z)
+{
+    return x += y + z;
+}
---
+++ cmake-build-debug/CMakeFiles/C__Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__Tutorial.dir/main.cpp.o"
+  "CMakeFiles/C__Tutorial.dir/basicConcepts.cpp.o"
+  "C__Tutorial.pdb"
+  "C__Tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
